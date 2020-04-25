ba0a0e15e44eef1e9ec4aee2bdf298ddb67d238e
Luc Mosser <luc.mosser86@orange.fr>
2020-04-25 12:02:10 +0200

    adding an upload page

--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,56 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react';
+import{ Typography, Button, Form, message, Input, Icon } from 'antd';
+
+const { Title } =  Typography;
+const { TextArea } = Input;
+
+const Continents = [
+  {key: 1, value: "Africa"},
+  {key: 2, value: "Europe"},
+  {key: 3, value: "North America"},
+  {key: 4, value: "South America"},
+  {key: 5, value: "Asia"}
+]
 
  function UploadProductPage() {
+
+    const [TitleValue, setTitleValue] = useState("")
+    const [DescriptionValue, setDescriptionValue] = useState("")
+    const [PriceValue, setPriceValue] = useState(0)
+
+    const onTitleChange = (e) => {
+      setTitleValue(e.currentTarget.value)
+    }
+
+    const onDescriptionChange = (e) => {
+      setDescriptionValue(e.currentTarget.value)
+    }
+
+    const onPriceChange = (e) => {
+      setPriceValue(e.currentTarget.value)
+    }
+
     return (
       <div style={{maxWidth:'700px', margin:'2rem auto'}}>
         <div style={{textAlign:'center', marginBottom:'2rem '}}>
-          <h2>Upload Pedals</h2>
+          <Title level={2}>Upload Pedals</Title>
         </div>
 
-        <form onSubmit >
+        <Form onSubmit >
 
         <br />
         <br />
 
         <label>Title</label>
-        <input
-          onChange
-          value
+        <Input
+          onChange={onTitleChange}
+          value={TitleValue}
         />
 
         <label>Description</label>
-        <input
-          onChange
-          value
+        <TextArea
+          onChange={onDescriptionChange}
+          value={DescriptionValue}
         />
 
         <br />
         <br />
 
         <label>Price ($)</label>
-        <input
-          onChange
-          value
+        <Input
+          onChange={onPriceChange}
+          value={PriceValue}
           type='number'
         />
 
         <select>
-          <option
-            key
-            value
-          >
-          </option>
+          {Continents.map(item => (
+              <option key={item.key} value={item.key}>{item.value}</option>
+            )
+          )}
         </select>
 
         <br />
         <br />
 
-        <button
-          onClick>
+        <Button
+          onClick
+        >
           Submit
-        </button>
+        </Button>
 
-        </form>
+        </Form>
 
       </div>
     );
