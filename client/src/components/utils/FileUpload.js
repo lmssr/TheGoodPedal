2c081c6de9cba7cb874531ffd8384af88b1a8b12
Luc Mosser <luc.mosser86@orange.fr>
2020-04-26 15:02:48 +0200

    adding an upload page

---
+++ client/src/components/utils/FileUpload.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FileUpload() {
+    return (
+      <div style={{ display:'flex', justifyContent:'space-between' }}>
+        FileUpload
+      </div>
+    )
+  }
+
+export default FileUpload;
--- client/src/components/views/UploadProductPage/UploadProductPage.js
+++ client/src/components/views/UploadProductPage/UploadProductPage.js
@@ -1,87 +1,88 @@
 import React, { useState } from 'react';
 import{ Typography, Button, Form, message, Input, Icon } from 'antd';
+import FileUpload from '../../utils/FileUpload';
 
 const { Title } =  Typography;
 const { TextArea } = Input;
 
 const Continents = [
-  {key: 1, value: "Africa"},
-  {key: 2, value: "Europe"},
-  {key: 3, value: "North America"},
-  {key: 4, value: "South America"},
-  {key: 5, value: "Asia"}
+{key: 1, value: "Africa"},
+{key: 2, value: "Europe"},
+{key: 3, value: "North America"},
+{key: 4, value: "South America"},
+{key: 5, value: "Asia"}
 ]
 
- function UploadProductPage() {
+function UploadProductPage() {
 
-    const [TitleValue, setTitleValue] = useState("")
-    const [DescriptionValue, setDescriptionValue] = useState("")
-    const [PriceValue, setPriceValue] = useState(0)
+  const [TitleValue, setTitleValue] = useState("")
+  const [DescriptionValue, setDescriptionValue] = useState("")
+  const [PriceValue, setPriceValue] = useState(0)
 
-    const onTitleChange = (e) => {
-      setTitleValue(e.currentTarget.value)
-    }
+  const onTitleChange = (e) => {
+    setTitleValue(e.currentTarget.value)
+  }
 
-    const onDescriptionChange = (e) => {
-      setDescriptionValue(e.currentTarget.value)
-    }
+  const onDescriptionChange = (e) => {
+    setDescriptionValue(e.currentTarget.value)
+  }
 
-    const onPriceChange = (e) => {
-      setPriceValue(e.currentTarget.value)
-    }
+  const onPriceChange = (e) => {
+    setPriceValue(e.currentTarget.value)
+  }
 
-    return (
-      <div style={{maxWidth:'700px', margin:'2rem auto'}}>
-        <div style={{textAlign:'center', marginBottom:'2rem '}}>
-          <Title level={2}>Upload Pedals</Title>
-        </div>
+  return (
+    <div style={{maxWidth:'700px', margin:'2rem auto'}}>
+    <div style={{textAlign:'center', marginBottom:'2rem '}}>
+    <Title level={2}>Upload Pedals</Title>
+    </div>
 
-        <Form onSubmit >
+    <Form onSubmit >
+    <FileUpload />
+    <br />
+    <br />
 
-        <br />
-        <br />
+    <label>Title</label>
+    <Input
+    onChange={onTitleChange}
+    value={TitleValue}
+    />
 
-        <label>Title</label>
-        <Input
-          onChange={onTitleChange}
-          value={TitleValue}
-        />
+    <label>Description</label>
+    <TextArea
+    onChange={onDescriptionChange}
+    value={DescriptionValue}
+    />
 
-        <label>Description</label>
-        <TextArea
-          onChange={onDescriptionChange}
-          value={DescriptionValue}
-        />
+    <br />
+    <br />
 
-        <br />
-        <br />
+    <label>Price ($)</label>
+    <Input
+    onChange={onPriceChange}
+    value={PriceValue}
+    type='number'
+    />
 
-        <label>Price ($)</label>
-        <Input
-          onChange={onPriceChange}
-          value={PriceValue}
-          type='number'
-        />
+    <select>
+    {Continents.map(item => (
+      <option key={item.key} value={item.key}>{item.value}</option>
+      )
+    )}
+    </select>
 
-        <select>
-          {Continents.map(item => (
-              <option key={item.key} value={item.key}>{item.value}</option>
-            )
-          )}
-        </select>
+    <br />
+    <br />
 
-        <br />
-        <br />
+    <Button
+    onClick
+    >
+    Submit
+    </Button>
 
-        <Button
-          onClick
-        >
-          Submit
-        </Button>
+    </Form>
 
-        </Form>
-
-      </div>
+    </div>
     );
 }
 
