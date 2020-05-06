e5ba475697ccf3c3657b23c893dcac3916adf259
Luc Mosser <luc.mosser86@orange.fr>
2020-05-06 17:35:49 +0200

    images deleting

--- client/src/components/utils/FileUpload.js
+++ client/src/components/utils/FileUpload.js
@@ -1,60 +1,79 @@
-import React, { useState } from 'react';
+import React, { useState } from 'react'
 import Dropzone from 'react-dropzone';
 import { Icon } from 'antd';
 import Axios from 'axios';
-
 function FileUpload(props) {
 
-  const [Images, setImages] = useState([]);
+    const [Images, setImages] = useState([])
 
-  const onDrop = (files) => {
-    let formData = new FormData();
-    const config = {
-      header: {'content-type': 'multipart/form-data'}
+    const onDrop = (files) => {
+
+        let formData = new FormData();
+        const config = {
+            header: { 'content-type': 'multipart/form-data' }
+        }
+        formData.append("file", files[0])
+        //save the Image we chose inside the Node Server
+        Axios.post('/api/product/uploadImage', formData, config)
+            .then(response => {
+                if (response.data.success) {
+
+                    setImages([...Images, response.data.image])
+                    props.refreshFunction([...Images, response.data.image])
+
+                } else {
+                    alert('Failed to save the Image in Server')
+                }
+            })
     }
-    formData.append("file", files[0])
 
-    Axios.post('api/product/uploadImage', formData, config)
-    .then(response => {
-      if(response.data.success) {
 
-        setImages([...Images, response.data.image])
-        props.refreshFunction([...Images, response.data.image])
+    const onDelete = (image) => {
+        const currentIndex = Images.indexOf(image);
 
-      } else {
-        alert('Failed to save the image in Server')
-      }
-    })
-  }
+        let newImages = [...Images]
+        newImages.splice(currentIndex, 1)
+
+        setImages(newImages)
+        props.refreshFunction(newImages)
+    }
 
     return (
-      <div style={{ display:'flex', justifyContent:'space-between' }}>
-        <Dropzone
-          onDrop={onDrop}
-          multiple={false}
-          maxSize={80000000}
-        >
+        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+            <Dropzone
+                onDrop={onDrop}
+                multiple={false}
+                maxSize={800000000}
+            >
+                {({ getRootProps, getInputProps }) => (
+                    <div style={{
+                        width: '300px', height: '240px', border: '1px solid lightgray',
+                        display: 'flex', alignItems: 'center', justifyContent: 'center'
+                    }}
+                        {...getRootProps()}
+                    >
+                        {console.log('getRootProps', { ...getRootProps() })}
+                        {console.log('getInputProps', { ...getInputProps() })}
+                        <input {...getInputProps()} />
+                        <Icon type="plus" style={{ fontSize: '3rem' }} />
 
-        {({getRootProps, getInputProps}) => (
-          <div style={{
-            width:'300px', height:'240px', border:'1px solid lightgray',
-            display:'flex', alignItems:'center', justifyContent:'center'
-          }}
-            {...getRootProps()}
-          >
-              <input {...getInputProps()} />
-              <Icon type='plus' style={{fontSize:'3rem'}} />
-          </div>
-        )}
-        </Dropzone>
+                    </div>
+                )}
+            </Dropzone>
 
-          <div style={{width:'350px', height:'240px', display:'flex', overflowX:'scroll'}}>
-            <div onClick>
-              <img />
+            <div style={{ display: 'flex', width: '350px', height: '240px', overflowX: 'scroll' }}>
+
+                {Images.map((image, index) => (
+                    <div onClick={() => onDelete(image)}>
+                        <img style={{ minWidth: '300px', width: '300px', height: '240px' }} src={`http://localhost:5000/${image}`} alt={`productImg-${index}`} />
+                    </div>
+                ))}
+
+
             </div>
-          </div>
-      </div>
-    )
-  }
 
-export default FileUpload;
+        </div>
+    )
+}
+
+export default FileUpload
