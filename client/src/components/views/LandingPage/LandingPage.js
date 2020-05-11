ec67fddb541e469387f76f37faf057c95dd85fcd
Luc Mosser <luc.mosser86@orange.fr>
2020-05-11 12:11:16 +0200

    landng page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,23 +1,64 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import { Icon, Col, Card, Row } from 'antd';
+import ImageSlider from '../../utils/ImageSlider';
+
+const { Meta } = 'Card';
 
 function LandingPage() {
 
-    useEffect(() => {
-Axios.post('/api/product/getProducs')
-  .then(response => {
-    if(response.data.success) {
+  const [Products, setProducts] = useState([])
 
-    } else {
-      alert('Failed to fetch product datas')
-    }
-  })
+  useEffect(() => {
+    Axios.post('/api/product/getProducs')
+      .then(response => {
+        if(response.data.success) {
+          setProducts(response.data.products)
+
+          console.log(response.data.products)
+        } else {
+          alert('Failed to fetch product datas')
+        }
+      })
     }, [])
 
+    const renderCards = Products.map((product, index) => {
+      return <Col lg={6} md={8} xs={24}>
+            <Card
+                hoverable={true}
+                cover={<a href={`/product/${product._id}`} > <ImageSlider images={product.images} /></a>}
+            >
+                <Meta
+                    title={product.title}
+                    description={`$${product.price}`}
+                />
+            </Card>
+        </Col>
+    })
     return (
-        <>
-        </>
-    )
-}
+      <div style ={{ width: '75%', margin:'3rem auto' }}>
+        <div style={{ textAlign:'center' }}>
+          <h2>Let's travel anywhere <Icon type='rocket' /> </h2>
+        </div>
 
-export default LandingPage
+        {Products.length === 0 ?
+          <div style={{ display: 'flex', height: '300px', justifyContent: 'center', alignItems: 'center' }}>
+            <h2>No post yet...</h2>
+          </div> :
+          <div>
+            <Row gutter={[16, 16]}>
+
+              {renderCards}
+
+             </Row>
+          </div>
+        }
+        <br /><br />
+        <div style={{ display: 'flex', justifyContent: 'center' }}>
+          <button onClick>Load More</button>
+        </div>
+      </div>
+      )
+  }
+
+  export default LandingPage
--- server/routes/product.js
+++ server/routes/product.js
@@ -53,55 +53,13 @@ router.post("/uploadProduct", auth, (req, res) => {
 });
 
 
-router.post("/getProducts", (req, res) => {
+router.post("/getProducts", auth, (req, res) => {
 
-    let order = req.body.order ? req.body.order : "desc";
-    let sortBy = req.body.sortBy ? req.body.sortBy : "_id";
-    let limit = req.body.limit ? parseInt(req.body.limit) : 100;
-    let skip = parseInt(req.body.skip);
-
-    let findArgs = {};
-    let term = req.body.searchTerm;
-
-    for (let key in req.body.filters) {
-
-        if (req.body.filters[key].length > 0) {
-            if (key === "price") {
-                findArgs[key] = {
-                    $gte: req.body.filters[key][0],
-                    $lte: req.body.filters[key][1]
-                }
-            } else {
-                findArgs[key] = req.body.filters[key];
-            }
-        }
-    }
-
-    console.log(findArgs)
-
-    if (term) {
         Product.find(findArgs)
-            .find({ $text: { $search: term } })
-            .populate("writer")
-            .sort([[sortBy, order]])
-            .skip(skip)
-            .limit(limit)
             .exec((err, products) => {
                 if (err) return res.status(400).json({ success: false, err })
-                res.status(200).json({ success: true, products, postSize: products.length })
+                res.status(200).json({ success: true, products })
             })
-    } else {
-        Product.find(findArgs)
-            .populate("writer")
-            .sort([[sortBy, order]])
-            .skip(skip)
-            .limit(limit)
-            .exec((err, products) => {
-                if (err) return res.status(400).json({ success: false, err })
-                res.status(200).json({ success: true, products, postSize: products.length })
-            })
-    }
-
 });
 
 
